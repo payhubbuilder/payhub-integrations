ded24adc5d3e01791295dc0d2fef11f886d62ddf
Lon Sun <lsun@payhub.com>
2014-02-20 13:42:48 -0800

    Added existing oscommerce plugin.

---
+++ oscommerce/README.md
@@ -0,0 +1,24 @@
+=== PayHub Gateway Plugin ===
+Contributors: EJ Costiniano
+Website: http://payhub.com
+Tags: payment, gateway, credit card
+License: GNU
+
+
+=== Description ===
+PayHub's plugin for osCommerce version 2.3.  This plugin allows you to process credit card payments through osCommerce with PayHub.  Please note, a PayHub account is required through Central Payment to process transactions.  If you need an account please contact us at wecare@payhub.com and we can have one of our sales associates contact you.
+
+=== How to find your API credentials ===
+1. Log into PayHub's Virtual Terminal
+2. Click on Admin
+3. Under General heading, click on 3rd Party API.
+4. Copy down your Username, Password, and Terminal Id.  Please note the username and password is case sensitive.
+
+=== Installation ===
+(this assumes you have osCommerce installed)
+1. Download the zip file.
+2. Unzip and place the files in their appropriate locations.
+3. Activate the plugin in osCommerce.
+4. Fill in the credentials with your API credentials.
+
+You should now be able to checkout using PayHub in your shopping cart.
---
+++ oscommerce/catalog/includes/languages/english/modules/payment/payhub.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2007 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_PAYHUB_TEXT_TITLE', 'PayHub Credit Card Module');
+  define('MODULE_PAYMENT_PAYHUB_TEXT_PUBLIC_TITLE', 'Credit Cards (Powered by PayHub)');
+  define('MODULE_PAYMENT_PAYHUB_TEXT_DESCRIPTION', '<a href="https://www.payhub.com" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit PayHub.com Website</a> to get more information about our services.<br /><br />To use this plugin, a PayHub account is required.  Here are a few ways to obtain a PayHub account:<br /><br />1) Contact the developer/sales representative by emailing <a href="mailto:ej@swebdev.net?subject=osCommerce PayHub Application">ej@swebdev.net</a>.<br /><br />2) Go to <a href="http://swebdev.net/applications">http://swebdev.net/application</a> to fill out an online application.');
+  define('MODULE_PAYMENT_PAYHUB_CREDIT_CARD_OWNER', 'Credit Card Owner:');
+  define('MODULE_PAYMENT_PAYHUB_CREDIT_CARD_NUMBER', 'Credit Card Number:');
+  define('MODULE_PAYMENT_PAYHUB_CREDIT_CARD_EXPIRES', 'Credit Card Expiry Date:');
+  define('MODULE_PAYMENT_PAYHUB_CREDIT_CARD_CVC', 'Credit Card Check Number (CVC):');
+  define('MODULE_PAYMENT_PAYHUB_ERROR_TITLE', 'There has been an error processing your credit card');
+  define('MODULE_PAYMENT_PAYHUB_ERROR_GENERAL', 'Please try again and if problems persist, please try another payment method.');
+  define('MODULE_PAYMENT_PAYHUB_ERROR_DECLINED', 'This credit card transaction has been declined. Please try again and if problems persist, please try another credit card or payment method.');
+  define('MODULE_PAYMENT_PAYHUB_ERROR_INVALID_EXP_DATE', 'The credit card expiration date is invalid. Please check the card information and try again.');
+  define('MODULE_PAYMENT_PAYHUB_ERROR_EXPIRED', 'The credit card has expired. Please try again with another card or payment method.');
+  define('MODULE_PAYMENT_PAYHUB_ERROR_CVC', 'The credit card check number (CVC) is invalid. Please check the card information and try again.');
+  define('MODULE_PAYMENT_PAYHUB_ERROR_FUNDS', 'Your credit card has been declined due to insufficient funds.');
+  define('MODULE_PAYMENT_PAYHUB_ERROR_CARD', 'You entered the incorrect card number, please try again.');
+?>
